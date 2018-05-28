f309bbdc006f7a3e3939ae8aad33b46874a04d6f
sja@voxxed.com <sja@voxxed.com>
2018-05-28 12:05:44 +0200

    - Use invoice Number in email subject on Reservation waiting payment page - Use invoice Number for confirmation email

--- src/main/java/alfio/manager/TicketReservationManager.java
+++ src/main/java/alfio/manager/TicketReservationManager.java
@@ -494,7 +494,7 @@ public class TicketReservationManager {
         }
 
         notificationManager.sendSimpleEmail(event, ticketReservation.getEmail(), messageSource.getMessage("reservation-email-subject",
-                new Object[]{getShortReservationID(event, reservationId), event.getDisplayName()}, language),
+                new Object[]{ticketReservation.getInvoiceNumber()}, language),
             () -> templateManager.renderTemplate(event, TemplateResource.CONFIRMATION_EMAIL, reservationEmailModel, language), attachments);
     }
 
